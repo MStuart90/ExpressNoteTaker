226d6d45a922b9a73be3338d7179259144cb95fc
Melanie Stuart <mmooradian90@yahoo.com>
2020-11-21 17:00:07 -0500

    adding routes

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,11 +1,10 @@
-//dependencies
+
 const path = require("path");
 const mainDir = path.join(__dirname, "/public");
 
 module.exports = (app) => {
-    //HTML route for note page
+  
     app.get("/notes", (req, res) => res.sendFile(path.join(mainDir, "notes.html")));
 
-    //HTML route for main page
     app.get("/", (req, res) => res.sendFile(path.join(mainDir, "index.html")));
 };
